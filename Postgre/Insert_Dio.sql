e04f34788b546c19c8612eed10633bddaf38047f
MSJantana <trio.mar@gmail.com>
2021-08-06 16:17:13 -0300

    Curso de Postre - Atualização 3 Tabelas Bancos/Tipo_Transacao

--- Postgre/Insert_Dio.sql
+++ Postgre/Insert_Dio.sql
@@ -10,7 +10,7 @@ INSERT INTO public.banco(numero, nome)VALUES (1,'Banco Bankpar S.A.');
 INSERT INTO public.banco(numero, nome)VALUES (740,'Banco Barclays S.A.');
 INSERT INTO public.banco(numero, nome)VALUES (107,'Banco BBM S.A.');
 
---select * from agencia
+--select * from cliente_transacoes
 
 --Populando a Tabela Tipo_Transação
 INSERT INTO public.tipo_transacao (nome) VALUES ('Débito');
@@ -83,3 +83,178 @@ INSERT INTO public.agencia(banco_numero, numero, nome)VALUES (107,2,'Agência N�
 INSERT INTO public.agencia(banco_numero, numero, nome)VALUES (107,3,'Agência Número 3 Banco BBM S.A.');
 INSERT INTO public.agencia(banco_numero, numero, nome)VALUES (107,4,'Agência Número 4 Banco BBM S.A.');
 INSERT INTO public.agencia(banco_numero, numero, nome)VALUES (107,5,'Agência Número 5 Banco BBM S.A.');
+
+--Populando a Tabela Conta Corrente
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (654, 1, 187511463, 1, 1);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (654, 2, 400962468, 2, 2);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (654, 3, 409112000, 3, 3);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (654, 4, 198474553, 7, 4);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (654, 5, 414448303, 0, 5);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (246, 1, 11857381, 6, 6);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (246, 2, 371047745, 5, 7);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (246, 3, 225416342, 6, 8);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (246, 4, 89940236, 1, 9);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (246, 5, 168215886, 1, 10);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (25, 1, 295739455, 5, 11);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (25, 2, 402464325, 6, 12);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (25, 3, 59256727, 5, 1);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (25, 4, 11718435, 5, 2);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (25, 5, 343352649, 6, 3);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (641, 1, 363352649, 6, 4);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (641, 2, 94265006, 2, 5);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (641, 3, 261507272, 1, 6);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (641, 4, 141758207, 2, 7);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (641, 5, 400575456, 1, 8);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (213, 1, 127918587, 3, 9);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (213, 2, 257406629, 2, 10);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (213, 3, 146124300, 0, 11);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (213, 4, 281590938, 4, 12);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (213, 5, 78358192, 0, 1);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (19, 1, 319089332, 7, 2);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (19, 2, 288218636, 5, 3);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (19, 3, 271352669, 7, 4);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (19, 4, 133151725, 5, 5);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (19, 5, 158562267, 0, 6);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (29, 1, 260070721, 1, 7);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (29, 2, 358751672, 0, 8);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (29, 3, 212379546, 7, 9);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (29, 4, 427367751, 8, 10);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (29, 5, 147091146, 5, 11);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (1, 1, 293238749, 6, 12);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (1, 2, 268004601, 2, 1);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (1, 3, 93598696, 7, 2);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (1, 4, 153273940, 8, 3);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (1, 5, 284779153, 0, 4);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (740, 1, 262879112, 2, 5);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (740, 2, 353286653, 8, 6);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (740, 3, 403521640, 1, 7);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (740, 4, 1745250, 4, 8);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (740, 5, 162401264, 7, 9);
+
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (107, 1, 262879112, 2, 10);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (107, 2, 146565516, 7, 11);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (107, 3, 109393292, 2, 12);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (107, 4, 94832614, 5, 1);
+INSERT INTO public.conta_corrente(banco_numero, agencia_numero, numero, digito, cliente_numero)	VALUES (107, 5, 106124891, 2, 2);
+
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (654, 1, 187511463, 1, 1, 1, 0.87);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (654, 2, 400962468, 2, 2, 2, 1.67);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (654, 3, 409112000, 3, 3, 3, 111.67);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (654, 4, 198474553, 7, 4, 4, 151.65);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (654, 5, 414448303, 0, 5, 1, 151.65);
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (246, 1, 11857381, 6, 6, 2, 51.65);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (246, 2, 371047745, 5, 7, 3, 351.45);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (246, 3, 225416342, 6, 8, 4, 1500.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (246, 4, 89940236, 1, 9, 1, 500.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (246, 5, 168215886, 1, 10, 2, 700.00);	
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (25, 1, 295739455, 5, 11, 1, 749.00);		
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (25, 2, 402464325, 6, 12, 2, 79.00);		
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (25, 3, 59256727, 5, 1, 3, 99.00);		
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (25, 4, 11718435, 5, 2, 4, 200.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (25, 5, 343352649, 6, 3, 1, 200.00);	
+
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (641, 1, 363352649, 6, 4, 2, 300.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (641, 2, 94265006, 2, 5, 3, 450.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (641, 3, 261507272, 1, 6, 4, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (641, 4, 141758207, 2, 7, 1, 150.00);		
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (641, 5, 400575456, 1, 8, 2, 250.00);		
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (213, 1, 127918587, 3, 9, 3, 550.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (213, 2, 257406629, 2, 10, 4, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (213, 3, 146124300, 0, 11, 1, 650.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (213, 4, 281590938, 4, 12, 2, 950.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (213, 5, 78358192, 0, 1, 3, 850.00);
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (19, 1, 319089332, 7, 2, 4, 550.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (19, 2, 288218636, 5, 3, 1, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (19, 3, 271352669, 7, 4, 2, 650.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (19, 4, 133151725, 5, 5, 3, 950.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (19, 5, 158562267, 0, 6, 4, 850.00);
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (29, 1, 260070721, 1, 7, 1, 550.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (29, 2, 358751672, 0, 8, 2, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (29, 3, 212379546, 7, 9, 3, 650.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (29, 4, 427367751, 8, 10, 4, 950.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (29, 5, 147091146, 5, 11, 1, 850.00);	
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (1, 1, 293238749, 6, 12, 2, 550.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (1, 2, 268004601, 2, 1, 3, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (1, 3, 93598696, 7, 2, 4, 650.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (1, 4, 153273940, 8, 3, 1, 950.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (1, 5, 284779153, 0, 4, 2, 850.00);	
+	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (740, 1, 262879112, 2, 5, 3, 550.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (740, 2, 353286653, 8, 6, 4, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (740, 3, 403521640, 1, 7, 1, 650.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (740, 4, 1745250, 4, 8, 2, 950.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (740, 5, 162401264, 7, 9, 3, 850.00);	
+
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (107, 1, 262879112, 2, 10, 4, 550.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (107, 2, 146565516, 7, 11, 1, 50.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (107, 3, 109393292, 2, 12, 2, 650.00);	
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (107, 4, 94832614, 5, 1, 1, 950.00);
+INSERT INTO public.cliente_transacoes(banco_numero, agencia_numero, conta_corrente_numero, conta_corrente_digito, cliente_numero, tipo_transacao_id, valor)
+	VALUES (107, 5, 106124891, 2, 2, 3, 850.00);
+
+
+
+	
