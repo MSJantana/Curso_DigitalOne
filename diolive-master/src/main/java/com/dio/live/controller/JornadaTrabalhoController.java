a378a62aa6061902bce3a8fed6cd503a6b81cd7c
MSJantana <trio.mar@gmail.com>
2021-09-14 14:38:28 -0300

    Adicionar Arquivos Spring Boot Controle de Acesso

--- diolive-master/src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
+++ diolive-master/src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -1,11 +1,11 @@
 package com.dio.live.controller;
 
-import com.dio.live.exception.PersonNotFoundException;
 import com.dio.live.model.JornadaTrabalho;
 import com.dio.live.service.JornadaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -28,7 +28,7 @@ public class JornadaTrabalhoController {
     }
 
     @GetMapping("/{idJornada}")
-    public ResponseEntity<JornadaTrabalho>  getJornadaByID(@PathVariable("idJornada") Long idJornada) throws PersonNotFoundException {
+    public ResponseEntity<JornadaTrabalho>  getJornadaByID(@PathVariable("idJornada") Long idJornada) throws Exception {
         return  ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found!")));
 
     }
@@ -38,15 +38,18 @@ public class JornadaTrabalhoController {
         return jornadaService.updateJornada(jornadaTrabalho);
     }
 
-    @DeleteMapping("/{idJornada}")
-    public ResponseEntity<JornadaTrabalho> deleteById(@PathVariable("idJornada") Long idJornada) throws PersonNotFoundException {
-        try {
-            jornadaService.deleteJornada(idJornada);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        return (ResponseEntity) ResponseEntity.ok();
+//    @DeleteMapping("/{idJornada}")
+//    public ResponseEntity deleteByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+//       try {
+//           jornadaService.deleteJornada(idJornada);
+//       }catch (Exception e){
+//           System.out.println(e.getMessage());
+//       }
+//        return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
+
+    @DeleteMapping("/{idjornada}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteByID(@PathVariable Long idjornada)throws Exception{
+        jornadaService.deleteJornada(idjornada);
     }
-
 }
-
